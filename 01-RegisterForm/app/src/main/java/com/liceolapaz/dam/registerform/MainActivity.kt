220ad2367268e956a05b1d2b6c778ea4a76bd077
carlos.mezquita <mezquitalobato@gmail.com>
2022-10-03 23:03:34 +0200

    spinner finished

--- 01-RegisterForm/app/src/main/java/com/liceolapaz/dam/registerform/MainActivity.kt
+++ 01-RegisterForm/app/src/main/java/com/liceolapaz/dam/registerform/MainActivity.kt
@@ -2,10 +2,24 @@ package com.liceolapaz.dam.registerform
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var countrySelector : Spinner
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        countrySelector = findViewById(R.id.country_selector)
+
+        val adaptador =
+            ArrayAdapter.createFromResource(this, R.array.valores_array, android.R.layout.simple_spinner_item)
+
+        adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        countrySelector.adapter = adaptador
     }
 }
